03fad3dda100aabeb58791cb51e80e7391a75a3c
Tukkos <jpfcastro@id.uff.br>
2022-09-15 12:00:27 -0300

    feat: cart screen with e-commerse values, total and login/finalizar compras button

--- src/CartItems.js
+++ src/CartItems.js
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 
-export default function CartItems({productName, price, descryption, image}) {
-    console.log(image);
+export default function CartItems({productName, price, descryption, image, quantity}) {
     return (
         <CartItemCard>
             <div className="itemWindow">
                 <div className="imgBox">
                     <img src={image} alt={productName}></img>
                 </div>
-                <div className="descBox">
+                <div className="nameDescryptionBox">
                     <div>
                         {productName}
                     </div>
@@ -16,8 +15,13 @@ export default function CartItems({productName, price, descryption, image}) {
                         {descryption}
                     </div>
                 </div>
-                <div>
-                    {price}
+                <div className="priceQuantityBox">
+                    <div>
+                        <box className="ionBox"><ion-icon name="add-outline"></ion-icon></box>
+                        <box className="ionBox">{quantity}</box>
+                        <box className="ionBox"><ion-icon name="remove-outline"></ion-icon></box>
+                    </div>
+                    <div>{price}</div>
                 </div>
             </div>
         </CartItemCard>
@@ -29,12 +33,11 @@ const CartItemCard = styled.div`
 .itemWindow {
     width: 90%;
     height: 100px;
-    background-color: #ffffff;
+    background-color: #F0F0F0;
     font-family: 'Graduate', cursive;
-    color: #869187;
     border-radius: 10px;
     margin-left: 30px;
-    margin-bottom: 20px;
+    margin-bottom: 15px;
     padding: 10px 20px 10px 20px;
 
     display: flex;
@@ -42,14 +45,40 @@ const CartItemCard = styled.div`
 }
 .imgBox {
     height: 80px;
-    height: 80px;
+    width: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 .imgBox > img {
     height: 80px;
-    width: auto;
 }
-.descBox {
-    display: row;
-    /* flex-direction: column; */
+.nameDescryptionBox {
+    width: 70%;
+    color: #869187;
+}
+.nameDescryptionBox > div {
+    margin-top: 10px;
+}
+.priceQuantityBox {
+    margin-top: 15px;
+    height: 60px;
+    width: 60px;
+}
+.priceQuantityBox > div {
+    margin-bottom: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.ionBox {
+    border: 2px solid #DFDFD5;
+    border-radius: 5px;
+    margin: 2px 2px 2px 2px;
+}
+ion-icon {
+    height: 15px;
+    width: 15px;
+    color: #DFDFD5;
 }
 `;
